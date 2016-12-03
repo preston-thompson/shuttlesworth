1e2ea61d68161a08f50c6660d9c6ec97c8591953
Preston Thompson <pthompson93@gmail.com>
2016-12-02 21:06:59 -0500

    Prefer shorter sentences according to a geometric distribution.

--- markov.py
+++ markov.py
@@ -4,6 +4,16 @@ word_db = list()
 end_word_db = list()
 start_word_db = list()
 
+def rand_geometric(min, max, alpha):
+    z = (alpha ** (max + 1)) - 1
+    c = random.randint(0, z - 1)
+    start = min
+    for i in range(0, max + 1):
+        end = start + alpha ** (max - i)
+        if start <= c and c < end:
+            return i
+        start = end
+
 def record(text):
     words = text.split()
     start_word_db.append(words[0])
@@ -23,13 +33,13 @@ def talk(word, max_length):
         word = random.choice(start_word_db)
     message = word + " "
     while len(message) < max_length:
-        depth = random.randint(0, len(word_db) - 1)
+        depth = rand_geometric(0, len(word_db) - 1, 2)
         while word not in word_db[depth] and depth >= 0:
             depth -= 1
         if depth < 0:
             break
         if word in end_word_db:
-            if random.randint(0, 1):
+            if random.randint(0, 4) == 0:
                 break
         next_words = random.choice(word_db[depth][word])
         for w in next_words:
